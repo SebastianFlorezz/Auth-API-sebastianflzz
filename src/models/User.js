f823ea64b9bd92b929f175ea147476e885a07f28
Sebastian Florez <120860160+SebastianFlorezz@users.noreply.github.com>
2025-09-23 12:50:55 -0500

    Model user changed with regrex validations and custom messages

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,33 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    email: {type: String, required: true, unique: true, lowercase: true, trim: true},
-    password: {type: String, required: true}
+    email: {
+        type: String, 
+        required: true, 
+        unique: true, 
+        lowercase: true, 
+        trim: true,
+        match: [
+             /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+             "Please fill with a valid email address"
+        ]
+    },
+
+
+    password: {
+        type: String, 
+        required: [true, "Password is required"],
+        minLength: [8, "Password must be at least 8 characters long"],
+        match: [
+            /^(?=.*[A-Z])(?=.*\d).+$/,
+            "Password must contain at least one uppercase letter and one number"
+        ]},
+    
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
 }, {timestamps: true});
 
 module.exports = mongoose.model("User", userSchema);
