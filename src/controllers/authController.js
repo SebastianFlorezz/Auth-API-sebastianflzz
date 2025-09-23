7e8b681d981f530de60bb1e6df225eb97912e9a1
Sebastian Florez <120860160+SebastianFlorezz@users.noreply.github.com>
2025-09-23 08:30:01 -0500

    register controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,2 +1,44 @@
-const express = require("express");
-const app = express();
+const User = require("../models/User.js");
+
+
+
+
+//register an User
+const register = async (req, res) => {
+
+    try{
+        const {email, password} = req.body;
+
+        //we verify if the email exists
+        const emailExists = await User.findOne({email});
+
+        // if email exist
+        if (emailExists){
+            return res.status(400).json({
+                error: "User already exist"
+            })
+        }
+
+        
+        // we save in the database
+        const newUser = await User.create({email, password});
+
+        res.status(201).json({
+            message: "User created succesfully" // TASK: fill the json with more attributes
+        })
+    } catch(err) {
+        res.status(500).json({
+            error: "Register error"
+        })
+    }
+}
+
+
+
+// login an User
+
+const login = async (req, res) => {
+
+}
+
+module.exports = {register, login}
