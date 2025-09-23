f2a536298c647ce9cbcfa26d16a76162845067a0
Sebastian Florez <120860160+SebastianFlorezz@users.noreply.github.com>
2025-09-23 11:36:24 -0500

    login controller with payload and jwt token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,7 @@
 const User = require("../models/User.js");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
 
 
 
@@ -44,6 +46,37 @@ const register = async (req, res) => {
 // login an User
 
 const login = async (req, res) => {
+    try{
+        const {email, password} = req.body;
+        
+        //we verify if the email exists in the database
+        const user = await User.findOne({email});
+
+        // if the user doesnt exists return 401 status
+        if(!user){
+            return res.status(401).json({ error: "Invalid credentials"});
+        };
+
+        // we compare the password given by the user with the password on the database
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch){
+            return res.status(401).json({error: "Invalid credentials"}) // TASK: fill the json with more attributes
+        }
+
+        //we define the payload for jwt
+        const payload = {
+            id: user._id,
+            email: user.email,
+        }
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: "1h"})
+        res.status(200).json({token, user: {id: user._id, email: user.email}})
+        
+
+    } catch(err){
+        console.error("error", (err))
+    }
 
 }
 
